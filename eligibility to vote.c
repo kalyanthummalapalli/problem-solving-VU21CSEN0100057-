5e590c636c5003d8723ad1de85a1edaf91f53711
kalyanthummalapalli <93921862+kalyanthummalapalli@users.noreply.github.com>
2021-11-27 10:34:54 +0530

    Create eligibility to vote.c

---
+++ eligibility to vote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter any number");
+	scanf("%d",&n);
+	if(n>=18)
+	{
+		printf("you are eligible to vote");
+	}
+	else
+	printf("you are not eligible to vote");
+}
