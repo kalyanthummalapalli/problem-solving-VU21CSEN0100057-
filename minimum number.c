6e782f31c492cc3bcb08e33fed3ab7b959ce6029
kalyanthummalapalli <93921862+kalyanthummalapalli@users.noreply.github.com>
2021-12-02 16:06:01 +0530

    Create minimum number.c

---
+++ minimum number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter any three numbers a,b,c");
+	scanf("%d%d%d",&a,&b,&c);
+	if (a>b)
+	{
+	printf("enter the minimum number is b");
+}
+	else if (b>c)
+	printf("enter the minimum number is c");
+	else
+	printf("the minimum number is a");
+	
+}
