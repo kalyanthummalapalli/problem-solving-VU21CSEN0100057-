9b1e16872b39b411a5ae446064a748b2a3bd2463
kalyanthummalapalli <93921862+kalyanthummalapalli@users.noreply.github.com>
2021-12-02 16:08:05 +0530

    Create maximum number.c

---
+++ maximum number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter any three numbers a,b,c");
+	scanf("%d%d%d",&a,&b,&c);
+	if (a>b)
+	{
+	printf("enter the maximum number is a");
+}
+	else if (b>c)
+	printf("enter the maximum number is b");
+	else
+	printf("the maximum number is c");
+	
+}
