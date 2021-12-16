a20aaadd920407826ce900f45de1891c639feb24
kalyanthummalapalli <93921862+kalyanthummalapalli@users.noreply.github.com>
2021-12-16 15:47:21 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int i=1,f;
+	printf("to print the factorial sums");
+	scanf("%d",&f);
+	for (i=1;i<=10;i++)
+	
+	{
+		f=f*i;
+		printf("%d",f);
+		printf("\n");
+	}
+	return 0;
+}
